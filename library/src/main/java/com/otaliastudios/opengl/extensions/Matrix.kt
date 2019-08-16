e70719372924f097ee3dddfe845fab1a91d7eefe
Mattia Iavarone <mat.iavarone@gmail.com>
2019-08-16 15:26:46 +0200

    Improve matrix functions

--- library/src/main/java/com/otaliastudios/opengl/extensions/Matrix.kt
+++ library/src/main/java/com/otaliastudios/opengl/extensions/Matrix.kt
@@ -8,6 +8,7 @@ import java.nio.FloatBuffer
 private fun FloatArray.checkSize() {
     if (size != 16) throw RuntimeException("Need a 16 values matrix.")
 }
+
 fun FloatArray.makeIdentity(): FloatArray {
     checkSize()
     Matrix.setIdentityM(this, 0)
@@ -18,38 +19,43 @@ fun FloatArray.clear(): FloatArray {
     return makeIdentity()
 }
 
+fun FloatArray.translate(x: Float = 0F, y: Float = 0F, z: Float = 0F): FloatArray {
+    checkSize()
+    Matrix.translateM(this, 0, x, y, z)
+    return this;
+}
+
+@Suppress("unused")
 fun FloatArray.translateX(translation: Float): FloatArray {
-    checkSize()
-    Matrix.translateM(this, 0, translation, 0F, 0F)
-    return this
+    return translate(x = translation)
 }
 
+@Suppress("unused")
 fun FloatArray.translateY(translation: Float): FloatArray {
-    checkSize()
-    Matrix.translateM(this, 0, 0F, translation, 0F)
-    return this
+    return translate(y = translation)
 }
 
+@Suppress("unused")
 fun FloatArray.translateZ(translation: Float): FloatArray {
+    return translate(z = translation)
+}
+
+fun FloatArray.scale(x: Float = 1F, y: Float = 1F, z: Float = 1F): FloatArray {
     checkSize()
-    Matrix.translateM(this, 0, 0F, 0F, translation)
+    Matrix.scaleM(this, 0, x, y, z)
     return this
 }
 
 fun FloatArray.scaleX(scale: Float): FloatArray {
-    checkSize()
-    Matrix.scaleM(this, 0, scale, 1F, 1F)
-    return this
+    return scale(x = scale)
 }
 
 fun FloatArray.scaleY(scale: Float): FloatArray {
-    checkSize()
-    Matrix.scaleM(this, 0, 1F, scale, 1F)
-    return this
+    return scale(y = scale)
 }
 
+@Suppress("unused")
 fun FloatArray.scaleZ(scale: Float): FloatArray {
-    checkSize()
-    Matrix.scaleM(this, 0, 1F, 1F, scale)
-    return this
+    return scale(z = scale)
+
 }
